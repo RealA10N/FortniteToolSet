9bcfc2bc90f063567c83bb57cdc9c7bc71996574
Alon <downtown2u@gmail.com>
2019-05-06 21:39:56 +0300

    Removed comments

--- FortniteToolSet.py
+++ FortniteToolSet.py
@@ -64,24 +64,9 @@ root = ProgramGUI()
 root.mainloop()
 
 
-''' THE MENU BAR! COMING SOON (;
-
-def AboutPopup():
-    filewin = tk.Toplevel(root)
-    button = tk.Button(filewin, text="About")
-    button.pack()
 
 
-root = tk.Tk()
-menubar = tk.Menu(root)
-filemenu = tk.Menu(menubar, tearoff=0)
-menubar.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="About", command=AboutPopup)
 
-filemenu.add_separator()
 
-filemenu.add_command(label="Exit", command=root.quit)
 
-root.config(menu=menubar)
-root.mainloop()
-'''
+
